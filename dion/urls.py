00be4f107925ab5f6b2f5f2d75caa4327115bc1c
MySecoundLanguage <mmamunism@gmail.com>
2022-06-23 19:00:01 +0600

    Added api docs

--- dion/urls.py
+++ dion/urls.py
@@ -15,8 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/api/v1/', include('core.api.v1.urls'))
+]
+
+
+
+API_INFO = openapi.Info(
+    title="dion API",
+    default_version="v1",
+    description="API documentation for dion App",
+)
+
+API_DOCS_SCHEMA_VIEWS = get_schema_view(
+    API_INFO,
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+urlpatterns += [
+    path("api-docs/", API_DOCS_SCHEMA_VIEWS.with_ui("swagger", cache_timeout=0), name="api_playground")
 ]
