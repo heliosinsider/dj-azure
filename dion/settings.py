4a61dcd7c0028803f5adaa93898e2b58695b01a4
MySecoundLanguage <mmamunism@gmail.com>
2022-06-23 18:39:48 +0600

    Added django rest

--- dion/settings.py
+++ dion/settings.py
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
